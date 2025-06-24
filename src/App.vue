ed5e8fd6b58bf343921e7d57d9eeffd49ad86939
j-swaroop <jyothiswaroop654@gmail.com>
2025-06-24 15:23:35 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterView, useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useCommon } from '@/stores/common.store.js';
+import { useRemIndicator } from '@/composables/remIndicatorComposable';
+import CircularProgressBar from '@/components/sharedComponents/CircularProgressBar.vue';
+
+const useCommonStore = useCommon();
+let { commonLoaderState } = storeToRefs(useCommonStore);
+
+const route = useRoute();
+const init = () => {
+  // Call any API call here that needs to happen on app initialization
+};
+
+// Initialize REM indicator functionality
+const { remIndicatorRef } = useRemIndicator();
+
+init();
+</script>
+
+<template>
+  <div class="app-wrapper">
+    <RouterView :key="route.fullPath" />
+    <CircularProgressBar v-if="commonLoaderState"></CircularProgressBar>
+  </div>
+  <div id="remIndicator" ref="remIndicatorRef"></div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Unbounded:wght@200..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+.app-wrapper {
+  height: 100%;
+  width: 100%;
+}
+
+#remIndicator {
+  position: absolute;
+  top: -10000px;
+  left: -10000px;
+  background: transparent;
+  opacity: 0;
+  width: 1rem;
+  height: 1rem;
+  z-index: 1;
+  pointer-events: none;
+  visibility: hidden;
+}
+</style>
---
+++ src/views/HomeWrapper.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { inject } from 'vue';
+
+const rem = inject('remToPixel');
+
+</script>
+
+<template>
+  <div class="home-page-wrapper">
+    {{ rem }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-page-wrapper {
+  height: 100%;
+  width: 100%;
+  display: grid;
+  overflow: hidden;
+  color: $color-red;
+}
+</style>
